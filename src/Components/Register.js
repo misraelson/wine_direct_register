80a52ad9b23a7f87b33e3a23bf6649f506852717
misraelson <misraelson@gmail.com>
2018-11-07 15:20:13 -0800

    basic price name and weight input for item

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -4,25 +4,49 @@ import Item from './Item';
 
 class Register extends React.Component {
   state = {
-    total_price: null,
-    price: null,
-    weight: null,
+    total_price: 0,
+    price: "",
     name: "",
+    weight: "",
   }
 
-  handleInput = event => {
+  handlePriceInput = (event) => {
+    let inputPrice = event.target.name;
+    console.log("First call", inputPrice)
+    inputPrice = event.target.value;
+    console.log("Second call", inputPrice)
+    this.setState({ price: inputPrice })
+    console.log("price state", this.state.price)
+  }
+
+  handleNameInput = (event) => {
     let inputName = event.target.name;
     inputName = event.target.value;
     this.setState({ name: inputName })
+    console.log("name state", this.state.name)
   }
 
-  handleSubmit = event => {
+  handleWeightInput = (event) => {
+    let inputWeight = event.target.name;
+    console.log("First call", inputWeight)
+    inputWeight = event.target.value;
+    console.log("Second call", inputWeight)
+    this.setState({ weight: inputWeight })
+    console.log("weight state", this.state.weight)
+  }
+
+  handleSubmit = (event) => {
     event.preventDefault();
     // this 👇 is the form data (captured above in handleChange) that we are sending to our rails api
-    new Item(this.state.name)
-    console.log(Item)
+    let item = new Item(this.state.price, this.state.name, this.state.weight)
+    item.scan()
+    console.log(item)
     // this 👇 clears inputTitle of the input field form
-    this.setState({ name: "" });
+    this.setState({
+      price: 0,
+      name: "",
+      weight: 0,
+    });
   };
 
 
@@ -34,11 +58,29 @@ class Register extends React.Component {
         onSubmit={this.handleSubmit}
       >
         <Input
+          type="number"
+          name="inputPrice"
+          placeholder="enter the price"
+          required
+          value={this.state.price}
+          onChange={this.handlePriceInput}
+        />
+        <Input
           type="text"
           name="inputName"
+          placeholder="enter item name"
           required
           value={this.state.name}
-          onChange={this.handleInput}
+          onChange={this.handleNameInput}
+        />
+        <Input
+          type="number"
+          name="inputWeight"
+          placeholder="enter the weight"
+          placeholder="enter item weight"
+          required
+          value={this.state.weight}
+          onChange={this.handleWeightInput}
         />
         <div className="Button">
             <Button className="submitButton" type="submit">Add New Item</Button>
