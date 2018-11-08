9efba5c634061ac821994f4b89672bef94a17f43
misraelson <misraelson@gmail.com>
2018-11-07 18:10:17 -0800

    added functionality to apply a discount once total has crossed a certain threshold value, can only be applied once

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -8,6 +8,19 @@ class Register extends React.Component {
     price: "",
     name: "",
     weight: "",
+    discount_applied: false,
+  }
+
+  apply_discount = (event) => {
+    event.preventDefault();
+    // const ten_percent_discount_threshold = 200
+    // if(this.state.total_price >= ten_percent_discount_threshold){
+      this.setState( {
+        total_price: parseFloat(this.state.total_price) - parseFloat( this.state.total_price * .10 ),
+        discount_applied: true,
+      })
+    // }
+
   }
 
   scan = (price, name, weight) => {
@@ -20,17 +33,18 @@ class Register extends React.Component {
       {name: 'merlot', price: 8},
       {name: 'pinot noir', price: 12},
     ]
-    const result = inventory.find( wine => wine.name === name );
-    console.log("result!", result)
+    // this is the result of a match search of inventory with the inputName
+    const result = inventory.find( (wine) => wine.name === name );
+
     if(price > 0) {
       this.setState( prevState => ({
         total_price: parseFloat(prevState.total_price) + parseFloat(price)
       }))
     }
     else if(weight > 0 && result != undefined) {
-        this.setState( (prevState) => ({
-          total_price: parseFloat(prevState.total_price) + parseFloat( weight * result.price )
-        }))
+      this.setState( (prevState) => ({
+        total_price: parseFloat(prevState.total_price) + parseFloat( weight * result.price )
+      }))
     }
     else{
       alert("Whoopsie! Something went terribly terribly wrong...you need to enter a price OR a wine name + weight OR you didn't enter anything at all!! It could also be the case that the wine you entered isn't in our inventory :(");
@@ -38,35 +52,36 @@ class Register extends React.Component {
   }
 
   handlePriceInput = (event) => {
-    let inputPrice = event.target.name;
-    console.log("First call", inputPrice)
-    inputPrice = event.target.value;
-    console.log("Second call", inputPrice)
+    // let inputPrice = event.target.name;
+    // console.log("First call", inputPrice)
+    let inputPrice = event.target.value;
+    // console.log("Second call", inputPrice)
     this.setState({ price: inputPrice })
-    console.log("price state", this.state.price)
+    // console.log("price state", this.state.price)
   }
 
   handleNameInput = (event) => {
     let inputName = event.target.name;
     inputName = event.target.value;
     this.setState({ name: inputName })
-    console.log("name state", this.state.name)
+    // console.log("name state", this.state.name)
   }
 
   handleWeightInput = (event) => {
     let inputWeight = event.target.name;
-    console.log("First call", inputWeight)
+    // console.log("First call", inputWeight)
     inputWeight = event.target.value;
-    console.log("Second call", inputWeight)
+    // console.log("Second call", inputWeight)
     this.setState({ weight: inputWeight })
-    console.log("weight state", this.state.weight)
+    // console.log("weight state", this.state.weight)
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    // this 👇 is the form data (captured above in handleChange) that we are sending to our rails api
+    // scan() is defined above and does the heavy lifting
     this.scan(this.state.price, this.state.name, this.state.weight)
-    // this 👇 clears inputTitle of the input field form
+    // this.apply_discount();
+    // this 👇 clears the form fields
     this.setState({
       price: "",
       name: "",
@@ -77,47 +92,62 @@ class Register extends React.Component {
 
 
   render() {
-    return(
-      <div className="register-main">
+    const ten_percent_discount_threshold = 200
+    if( this.state.total_price >= ten_percent_discount_threshold && this.state.discount_applied === false ) {
+      return(
         <Form
           className="Form"
-          onSubmit={this.handleSubmit}
+          onSubmit={this.apply_discount}
           >
-            <Input
-              type="number"
-              name="inputPrice"
-              placeholder="enter the price"
-              value={this.state.price}
-              onChange={this.handlePriceInput}
-            />
-            <Input
-              type="text"
-              name="inputName"
-              placeholder="enter item name"
-              value={this.state.name}
-              onChange={this.handleNameInput}
-            />
-            <Input
-              type="number"
-              name="inputWeight"
-              placeholder="enter the weight"
-              value={this.state.weight}
-              onChange={this.handleWeightInput}
-            />
-            <div className="Button">
-              <Button className="submitButton" type="submit">Add New Item</Button>
+        <div className="Button">
+          <Button className="submitButton" type="submit">Apply discount</Button>
+        </div>
+        </Form>
+      )
+    }
+    else{
+      return(
+        <div className="register-main">
+          <Form
+            className="Form"
+            onSubmit={this.handleSubmit}
+            >
+              <Input
+                type="number"
+                name="inputPrice"
+                placeholder="enter the price"
+                value={this.state.price}
+                onChange={this.handlePriceInput}
+              />
+              <Input
+                type="text"
+                name="inputName"
+                placeholder="enter item name"
+                value={this.state.name}
+                onChange={this.handleNameInput}
+              />
+              <Input
+                type="number"
+                name="inputWeight"
+                placeholder="enter the weight"
+                value={this.state.weight}
+                onChange={this.handleWeightInput}
+              />
+              <div className="Button">
+                <Button className="submitButton" type="submit">Add New Item</Button>
+              </div>
+            </Form>
+            <div>
+              <h1>
+                Total:
+              </h1>
+              <h2>
+                { this.state.total_price }
+              </h2>
             </div>
-          </Form>
-          <div>
-            <h1>
-              Total:
-            </h1>
-            <h2>
-              { this.state.total_price }
-            </h2>
           </div>
-      </div>
-    )
+        )
+    }
   }
 
 }
